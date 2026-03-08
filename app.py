3eb2c126becf308efafe0ad729de50eac511885b
Maya Becerra <mayabecerra@Mayas-MacBook-Air-153.local>
2026-03-08 16:07:28 -0700

    beginning to make routes for flask

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 """
 Main Backend for HealthTech! This file will initialize Flask
 """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
