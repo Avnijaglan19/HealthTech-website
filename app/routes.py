b162673b1ed6a7240c0eed74ba16bf49f958c923
Avni Jaglan <avnijaglan@Avnis-MacBook-Pro.local>
2026-03-12 00:29:40 -0700

    Updated login page and routing

--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,32 @@
 """
 Main Backend for HealthTech! This file will initialize Flask
 """
-from flask import Flask, render_template
-from . import app
-from app.forms import LoginForm  # Importing the LoginForm class from forms.py
+from flask import render_template, request, redirect, url_for
+from app import app
 
-# ------------------------------------------
-# Route for home page
-# Will render home.html when user visits "/"
-# ------------------------------------------
-@app.route("/")
+
+# LOGIN PAGE
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return redirect(url_for("home"))
+    return render_template("login.html")
+
+
+# HOME PAGE
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-# ------------------------------------------
-# Route for login
-# Will render login.html when user visits "/login"
-# ------------------------------------------ 
-@app.route("/login")
-def login():
-    form = LoginForm()
-    return render_template("login.html", form=form)
 
-# ------------------------------------------
-# Route for manual
-# Will handle the "get" and "post"
-# ------------------------------------------ 
-@app.route("/manual", methods=["GET", "POST"])
+# MANUAL PAGE
+@app.route("/manual")
 def manual():
     return render_template("manual.html")
 
-# ------------------------------------------
-# Route for virtual
-# Will render virtual.html
-# ------------------------------------------
+
+# VIRTUAL PAGE
 @app.route("/virtual")
 def virtual():
     return render_template("virtual.html")
-
--- healthtech.py
+++ healthtech.py
@@ -12,4 +12,9 @@
 
 from app import app
 
+
+if __name__ == "__main__":
+    # run on a different port if port 5000 is occupied
+    app.run(port=5008)
+
 # from member of the app package, import the app object and run the Flask application
